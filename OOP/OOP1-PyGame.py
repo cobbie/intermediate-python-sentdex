63d8c94616448a5642a8d88221a72d7e64ed5116
Cobbie Quintos <cobbiequintos@gmail.com>
2019-02-15 15:46:19 +0800

    lesson 13 OOP

---
+++ OOP/OOP1-PyGame.py
@@ -0,0 +1,17 @@
+import random
+import pygame
+
+class Bob:
+
+    def __init__(self, color):
+        self.x = random.randrange(0, WIDTH)
+        self.y = random.randrange(0, HEIGHT)
+        self.size = random.randrange(4,8)
+        self.color = color
+        
+    
+    def move(self):
+        self.move_x = random.randrange(-1,2)
+        self.move_y = random.randrange(-1,2)
+        self.x += self.move_x
+        self.y = self.move_y
